5f85200a75b57772754cbecf32c91416892b2f7b
Christian Unnerstall <me@christianunnerstall.com>
2023-01-16 11:12:30 +0100

    Changed routing

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 app.set( 'port', process.env.APP_PORT || 3000);
 app.use(express.json());
 
-app.use( '/', router );
+app.use( '/users', router );
 
 // In case no route matches
 app.use(( req,res ) => {
